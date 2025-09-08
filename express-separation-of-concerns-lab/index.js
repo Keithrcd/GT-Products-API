38b8a8de2341707f02732b762b9df4f7514a0ba6
Keithrcd <recedekeithlorenz@gmail.com>
2025-09-08 15:01:43 +0800

    Added patch route and comments functionality

--- express-separation-of-concerns-lab/index.js
+++ express-separation-of-concerns-lab/index.js
@@ -1,6 +1,8 @@
 // index.js
 import express from 'express';
-import postRoutes from './express-separation-of-concerns-lab/src/routes/post.routes.js';
+import postRoutes from './src/routes/post.routes.js';
+// *** IMPORT THE NEW COMMENT ROUTES ***
+import commentRoutes from './src/routes/comment.routes.js';
 
 const app = express();
 const port = 3000;
@@ -9,6 +11,9 @@ app.use(express.json());
 
 // Mount the post routes
 app.use('/posts', postRoutes);
+
+// *** MOUNT THE NEW COMMENT ROUTES ***
+app.use('/comments', commentRoutes);
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
